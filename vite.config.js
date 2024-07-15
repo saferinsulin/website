d02bbb930229b9ef72f931250ac6ea21ec38d992
JP Lomas <jp.lomas@gmail.com>
2024-07-15 09:02:18 +0100

    Run as PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toUTCString()),
   },
