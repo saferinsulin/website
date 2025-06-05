255aaa23a6a888ac0ab29333efd5cb10af01ff5e
JP Lomas <jp.lomas@gmail.com>
2025-06-05 13:24:01 +0100

    Education section, tab logic rework

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <TopMenu @changeMenu="tab => this.activeIndex = tab" :items="items" :activeIndex="activeIndex"></TopMenu>
+  <TopMenu @changeMenu="tab => this.activeIndex = tab" :activeIndex="activeIndex"></TopMenu>
   <SuiContainer>
     <div class="spacer"></div>
     <SuiHeader size="large">
@@ -12,9 +12,8 @@
       </SuiHeaderContent>
 
     </SuiHeader>
-    <MainTab v-if="activeIndex === 0" />
+    <MainTab v-if="activeIndex === 0 || activeIndex === 2" />
     <GovernanceTab v-if="activeIndex === 1" />
-    <ResourcesTab v-if="activeIndex === 2" />
   </SuiContainer>
   <Footer />
 </template>
@@ -26,18 +25,12 @@ import TopMenu from './components/TopMenu.vue';
 import Footer from './components/Footer.vue';
 import GovernanceTab from './components/GovernanceTab.vue';
 import MainTab from './components/MainTab.vue';
-import ResourcesTab from './components/ResourcesTab.vue';
 
 export default {
-  components: { TopMenu, Footer, MainTab, GovernanceTab, ResourcesTab },
+  components: { TopMenu, Footer, MainTab, GovernanceTab },
   setup() {
-    const items = [
-      'Calculator',
-      'Governance',
-      'Resources',
-    ];
     const activeIndex = ref(0);
-    return { items, activeIndex }
+    return { activeIndex }
   },
 }
 </script>
